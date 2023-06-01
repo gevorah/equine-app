2a4cd70819273167689d24e280127bc9de632fed
sebastianvilla17 <1193269834@u.icesi.edu.co>
2023-06-01 12:08:26 -0500

    appointments

--- app/src/main/java/co/edu/icesi/dev/equineapp/model/Appointment.kt
+++ app/src/main/java/co/edu/icesi/dev/equineapp/model/Appointment.kt
@@ -1,14 +1,13 @@
 package co.edu.icesi.dev.equineapp.model
 
-import java.util.*
-
 class Appointment (
     var id: String = "",
-    var owner: String = "",
+    var horseName: String = "",
     var address: String = "",
+    var phoneNumber: String = "",
     var date: String = "",
     var time: String = "",
     var reason: String = "",
-    var ownerId: String = "",
+    var userId: String = "",
     var horseId: String = "",
 )
